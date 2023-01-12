14f51f080790e5a7502a8b0c5d67ca905975bc3b
Dana Truong <tramtramphotos@gmail.com>
2023-01-12 11:11:25 -0600

    Add Card Component

--- src/Results/Results.js
+++ src/Results/Results.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import './Results.css'
+import Card from '../Card/Card'
 
 const Results = () => {
   return (
     <div>
       <h2>Results</h2>
+      <Card />
     </div>
   )
 }
