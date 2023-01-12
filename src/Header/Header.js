90543db91722d0a6a0bdf4d0dd9e6fff9d002097
Dana Truong <tramtramphotos@gmail.com>
2023-01-12 12:54:07 -0600

    Style Header Component

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -3,9 +3,14 @@ import './Header.css'
 
 const Header = () => {
   return (
-    <div className="header">
-      <h2>Header</h2>
-    </div>
+    <nav className="header">
+      <div className="logo-container">
+        <img className="logo" alt="Animal Crossing logo" src='https://upload.wikimedia.org/wikipedia/commons/7/7c/Animal_Crossing_Leaf.svg' />
+      </div>
+      <div className="link-container">
+        <a>About Us</a>
+      </div>
+    </nav>
   )
 }
 
