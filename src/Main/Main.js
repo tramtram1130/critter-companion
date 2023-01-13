03f061a1c7f96b3f05304a57ae649d758aaabb5e
Dana Truong <tramtramphotos@gmail.com>
2023-01-13 13:36:34 -0600

    Refactor and implement hooks

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,31 +1,38 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import './Main.css'
 import Form from '../Form/Form'
 import Results from '../Results/Results'
 
-class Main extends Component {
-  constructor() {
-    super()
-    this.state = {
-      bugs: [],
-      filteredCritters: [],
-      isLoading: false
+function Main() {
+
+  const [bugs, setBugs] = useState([])
+  const [filteredCritters, setFilteredCritters] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState("")
+
+  const getBugs = async () => {
+    const url = 'https://acnhapi.com/v1a/bugs'
+    setError("")
+    setIsLoading(true)
+    try {
+      const response = await fetch(url)
+      const critters = await response.json()
+      setBugs(critters)
+      setIsLoading(false)
+    } catch (error) {
+      setError(error.message)
     }
   }
 
-  componentDidMount = () => {
-    fetch('https://acnhapi.com/v1a/bugs')
-      .then(this.setState({ isLoading: true }))
-      .then(response => response.json())
-      .then(data => this.setState({ bugs: data, isLoading: false }))
-      .catch(err => console.log(err))
-  }
+  useEffect(() => {
+    getBugs()
+  }, [])
 
-  filterCritters = (month, hour, hemisphere) => {
+  const filterCritters = (month, hour, hemisphere) => {
     const selectedMonth = month ? parseInt(month) : undefined
     const selectedHour = hour ? parseInt(hour) : undefined
     const selectedHemisphere = hemisphere ? `month-array-${hemisphere}` : undefined
-    const filteredBuddies = this.state.bugs.filter(bug => {
+    const filteredBuddies = bugs.filter(bug => {
       if (!selectedMonth || !selectedHour || !selectedHemisphere) {
         return
       }
@@ -33,22 +40,22 @@ class Main extends Component {
         return bug
       }
     })
-    this.setState({ filteredCritters: filteredBuddies })
+    setFilteredCritters(filteredBuddies)
     return
   }
 
-  render() {
-    return (
-      <div className="main">
-        <h1 className="app-name">Critter Companion</h1>
-        <Form filterCritters={this.filterCritters}/>
-        <div className="home-gif-container">
-          <img className="home-gif" src="https://media.tenor.com/gxtnvT8-FnUAAAAi/acnh.gif" />
-        </div>
-        {!this.state.isLoading && <Results bugs={this.state.filteredCritters} />}
+
+  return (
+    <div className="main">
+      <h1 className="app-name">Critter Companion</h1>
+      <Form filterCritters={filterCritters} />
+      <div className="home-gif-container">
+        <img className="home-gif" alt="Dog Villager dancing" src="https://media.tenor.com/gxtnvT8-FnUAAAAi/acnh.gif" />
       </div>
-    )
-  }
+      {!isLoading && <Results bugs={filteredCritters} />}
+    </div>
+  )
+
 }
 
 export default Main
