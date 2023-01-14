cc05909b4a7e24b49c35faed4fc3ece3fe984f05
Dana Truong <tramtramphotos@gmail.com>
2023-01-14 14:49:20 -0600

    Install and lightly implement Micromodal

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -1,12 +1,26 @@
 import React from 'react'
 import './Card.css'
+import MicroModal from 'react-micro-modal'
 
-const Card = (props) => {
-  const { imgUrl } = props
+const Card = ({ imgUrl }) => {
+  console.log(imgUrl)
   return (
-    <div className="card">
-      <img src={imgUrl} />
-    </div>
+    <MicroModal
+      trigger={(open) => (
+        <div className="card">
+          <img onClick={open} src={imgUrl} />
+        </div>
+      )}
+    >
+      {(close) => (
+        <div className='modal'>
+          <h2>Modal</h2>
+          <button className='close-modal-button' onClick={close}>
+            Close!
+          </button>
+        </div>
+      )}
+    </MicroModal>
   )
 }
 
