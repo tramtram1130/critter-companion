71ac57768ea44fc1c1ede502dc94d267388ab750
Dana Truong <tramtramphotos@gmail.com>
2023-01-16 01:33:49 -0600

    Create Collection Component

---
+++ src/components/Collection/Collection.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Collection.css'
+
+const Collection = () => {
+  return (
+    <div>
+     <h2>My Collection</h2>
+    </div>
+  )
+}
+
+export default Collection
