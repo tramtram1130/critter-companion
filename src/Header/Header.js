8cca3002c22cc739180a2ad2a3117e6ce459e485
Dana Truong <tramtramphotos@gmail.com>
2023-01-16 01:13:40 -0600

    Style and refactor Header Component

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import './Header.css'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
     <nav className="header">
+      <Link to='/'>
       <div className="logo-container">
         <img className="logo" alt="Animal Crossing logo" src='https://upload.wikimedia.org/wikipedia/commons/7/7c/Animal_Crossing_Leaf.svg' />
       </div>
+      </Link>
       <div className="link-container">
-        <a>About Us</a>
+        <a>My Collection</a>
       </div>
     </nav>
   )
