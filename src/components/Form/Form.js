461948313307de58447f56c6c533d7c7b8202b29
Dana Truong <tramtramphotos@gmail.com>
2023-01-16 01:34:13 -0600

    Refactor Form Component

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,4 +1,4 @@
-import React, { Component, useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import './Form.css'
 import ribbon from '../../assets/images/ribbon-end-yellow.svg'
 
