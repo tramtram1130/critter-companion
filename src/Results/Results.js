dbb5ea5c067e4ced9d2cae970e4e3394a2aebe5f
Dana Truong <tramtramphotos@gmail.com>
2023-01-16 01:14:56 -0600

    Refactor Result Component props

--- src/Results/Results.js
+++ src/Results/Results.js
@@ -3,7 +3,19 @@ import './Results.css'
 import Card from '../Card/Card'
 
 const Results = (props) => {
-  const allBugs = props.bugs.map(bug => <Card key={bug.id} name={bug.name['name-USen']} imgUrl={bug.image_uri} fileName={bug['file-name']} price={bug.price} bio={bug['museum-phrase']} />)
+  const allBugs = props.bugs.map(bug => {
+    return (
+      <Card
+        key={bug.id}
+        id={bug.id}
+        name={bug.name['name-USen']}
+        imgUrl={bug.image_uri}
+        fileName={bug['file-name']}
+        price={bug.price}
+        bio={bug['museum-phrase']}
+      />
+    )
+  })
   return (
     <div className="results">
       {allBugs}
